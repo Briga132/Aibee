e6045a4e54709d951568ec092251186dd87aa514
24JS3 <24js3@TECHIN.INTERNAL>
2025-09-25 11:26:50 +0300

    read more...

--- script/script.js
+++ script/script.js
@@ -1,23 +1,57 @@
-
-  const slider = document.getElementById('slider');
-  const arrowL = document.getElementById('arrowL');
-  const arrowR = document.getElementById('arrowR');
-
-  function scrollLeft() {
-    console.log(slider)
-    console.log(slider.clientWidth)
-    slider.scrollBy(
-        {
-            left: -slider.clientWidth,
-            behavior: 'smooth'
-        }
-    );
+  $(".bobel").click(function()
+  {
+      $(".labuba2").toggle('show')
   }
-  function scrollRight() {
-    slider.scrollBy(
-        { 
-            left: slider.clientWidth, behavior: 'smooth' 
+  )
+
+
+    const button = document.querySelector('.bobel');
+    const dots = document.querySelector('.labuba3');
+    const fullText = document.querySelector('.labuba2');
+
+    button.addEventListener('click', function () {
+        const isVisible = fullText.classList.contains('show');
+
+        if (isVisible) {
+
+            dots.style.display = 'inline';
+            fullText.classList.remove('show');
+            button.textContent = 'Read More';
+        } else {
+
+            dots.style.display = 'none';
+            fullText.classList.add('show');
+            button.textContent = 'Read Less';
         }
-    );
-  }
+    });
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const buttons = document.querySelectorAll('.gore');
+
+        buttons.forEach(function (button) {
+            button.addEventListener('click', function () {
+                const card = button.closest('.kletka1, .kletka2, .kletka3, .kletka4');
+
+                const dots = card.querySelector('.help3');
+                const fullText = card.querySelector('.help2');
+
+                const isVisible = fullText.classList.contains('show');
+
+                if (isVisible) {
+                    // Скрываем текст
+                    dots.style.display = 'inline';
+                    fullText.classList.remove('show');
+                    button.textContent = 'READ MORE';
+                } else {
+                    // Показываем текст
+                    dots.style.display = 'none';
+                    fullText.classList.add('show');
+                    button.textContent = 'READ LESS';
+                }
+            });
+        });
+    });
+
 
