425dafb28b0f82bb009ba34d46e081b7080c8a2d
24JS3 <24js3@TECHIN.INTERNAL>
2025-09-30 09:14:34 +0300

    fix

--- script/contact.js
+++ script/contact.js
@@ -1,63 +1,3 @@
-  
-  // about hospital
-  $(".bobel").click(function()
-  {
-      $(".labuba2").toggle('show')
-  }
-  )
-
-
-    const button = document.querySelector('.bobel');
-    const dots = document.querySelector('.labuba3');
-    const fullText = document.querySelector('.labuba2');
-
-    button.addEventListener('click', function () {
-        const isVisible = fullText.classList.contains('show');
-
-        if (isVisible) {
-
-            dots.style.display = 'inline';
-            fullText.classList.remove('show');
-            button.textContent = 'Read More';
-        } else {
-
-            dots.style.display = 'none';
-            fullText.classList.add('show');
-            button.textContent = 'Read Less';
-        }
-    });
-
-
-// hospital treatment
-    document.addEventListener('DOMContentLoaded', function () {
-        const buttons = document.querySelectorAll('.gore');
-
-        buttons.forEach(function (button) {
-            button.addEventListener('click', function () {
-                const card = button.closest('.kletka1, .kletka2, .kletka3, .kletka4');
-
-                const dots = card.querySelector('.help3');
-                const fullText = card.querySelector('.help2');
-
-                const isVisible = fullText.classList.contains('show');
-
-                if (isVisible) {
-
-                    dots.style.display = 'inline';
-                    fullText.classList.remove('show');
-                    button.textContent = 'READ MORE';
-                } else {
-
-                    dots.style.display = 'none';
-                    fullText.classList.add('show');
-                    button.textContent = 'READ LESS';
-                }
-            });
-        });
-    });
-
-
-  // GET IN TOUCH
 document.getElementById('sendBtn').addEventListener('click', function(e) {
   e.preventDefault();
 
@@ -127,9 +67,3 @@ document.getElementById('sendBtn').addEventListener('click', function(e) {
     alert('Forma sėkmingai išsiųsta! Ačiū :)');
   }
 });
-
-// book apointment
-document.getElementById('btn').addEventListener('click', function() {
-  alert('Sėkmingai išsiųsta! Ačiū :)');
-});
-
